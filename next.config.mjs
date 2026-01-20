f3de6a5b0afff0b68768dfcc783718b0460b7217
irazirfan <irazirfan@gmail.com>
2026-01-20 14:18:10 +0600

    updated next.config, added dploy.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+// This checks if we are running the 'build' command
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'iONE_portfolio';
+
 const nextConfig = {
+  output: 'export',
+  // Only use the repo name prefix if we are in production (GitHub Pages)
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
